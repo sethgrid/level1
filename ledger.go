f85f3e8aa1b0397c49247ea7ce6f5389cf765a6e
Seth Ammons <seth.ammons@sendgrid.com>
2014-01-26 15:17:58 -0800

    ledger miner to use go routines. soooo slow.

--- ledger.go
+++ ledger.go
@@ -12,8 +12,10 @@ import (
 	"time"
 )
 
-func getHeadHash() []byte {
+func getHeadHash(i int) []byte {
+	iStr := strconv.Itoa(i)
 	cmd := exec.Command("git", "rev-parse", "HEAD")
+	cmd.Dir = "level1_" + iStr
 	head, err := cmd.Output()
 	if err != nil {
 		fmt.Println("Error getting head hash")
@@ -22,18 +24,78 @@ func getHeadHash() []byte {
 	return head[:len(head)-1]
 }
 
-func resetHead() {
+func resetHead(i int) {
+	iStr := strconv.Itoa(i)
 	cmd := exec.Command("git", "reset", "origin/HEAD", "--hard")
+	cmd.Dir = "level1_" + iStr
 	cmd.Run()
 }
 
-func gitCommit(msg string) {
+func gitCommit(i int, msg string) {
+	iStr := strconv.Itoa(i)
 	cmd := exec.Command("git", "commit", "-am", msg)
+	cmd.Dir = "level1_" + iStr
 	cmd.Run()
 }
 
-func getDifficulty() []byte {
-	f, err := os.Open("difficulty.txt")
+func gitReset(i int, hash string) {
+	iStr := strconv.Itoa(i)
+	cmd := exec.Command("git", "reset", "-hard", hash)
+	cmd.Dir = "level1_" + iStr
+	cmd.Run()
+}
+
+func gitAddLedger(i int) {
+	iStr := strconv.Itoa(i)
+	cmd := exec.Command("git", "add", "LEDGER.txt")
+	cmd.Dir = "level1_" + iStr
+	cmd.Run()
+}
+
+func getTree(i int) []byte {
+	iStr := strconv.Itoa(i)
+	cmd := exec.Command("git", "write-tree")
+	cmd.Dir = "level1_" + iStr
+	tree, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error getting git write-tree", err)
+	}
+	return tree
+}
+
+func getSha1(i int, body string) []byte {
+	iStr := strconv.Itoa(i)
+	cmd := exec.Command("git", "hash-object", "-t", "commit", "--stdin")
+	cmd.Dir = "level1_" + iStr
+
+	toWrite := body
+	cmd.Stdin = strings.NewReader(toWrite)
+
+	sha1, err := cmd.Output()
+	if err != nil {
+		fmt.Println("Error getting sha1", err)
+	}
+	return sha1
+}
+
+func clone() {
+	cmd := exec.Command("git", "clone", repo)
+	cmd.Run()
+}
+
+func copyRepo(i int) {
+	iStr := strconv.Itoa(i)
+	cmd := exec.Command("cp", "-r", "level1", "level1_"+iStr)
+	_, err := cmd.Output()
+	if err != nil {
+		fmt.Println("There was an error copying the repo", err)
+	}
+	fmt.Println("There should be level1_" + iStr)
+}
+
+func getDifficulty(i int) []byte {
+	iStr := strconv.Itoa(i)
+	f, err := os.Open("level1_" + iStr + "/difficulty.txt")
 	if err != nil {
 		fmt.Println("Error opening difficulty:", err)
 	}
@@ -47,14 +109,12 @@ func getDifficulty() []byte {
 	return line
 }
 
-func updateLedger() {
-	// just appends. Needs to read file, find if entry is there
-	// and increment, else append
-
+func updateLedger(i int) {
 	var newFileContent string
 	userFound := false
+	iStr := strconv.Itoa(i)
 
-	f, err := os.OpenFile("LEDGER.txt", os.O_RDWR, 0644)
+	f, err := os.OpenFile("level1_"+iStr+"/LEDGER.txt", os.O_RDWR, 0644)
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 	}
@@ -95,11 +155,6 @@ func updateLedger() {
 	f.Write([]byte(newFileContent))
 }
 
-func clone() {
-	cmd := exec.Command("git", "clone", repo)
-	cmd.Run()
-}
-
 var repo, user string
 
 func init() {
@@ -109,25 +164,61 @@ func init() {
 	flag.Parse()
 }
 
-func main() {
-
-	clone()
-	updateLedger()
+func mine(comm chan string, quit chan bool, i int, head, difficulty []byte) {
+	counter := 0
 	for {
-		gitCommit(fmt.Sprintf("I can haz gitcoin? %s", time.Now()))
+		select {
+		case <-quit:
+			return
+		default:
+		}
+		counter++
+		unix := time.Now().Unix()
 
-		//resetHead()
-		head := getHeadHash()
-		difficulty := getDifficulty()
-		ofInterest := head[:len(difficulty)]
+		tree := getTree(i)
 
-		fmt.Println("Current Hash\t" + string(head))
-		fmt.Println("Relevant Part\t" + string(ofInterest))
-		fmt.Println("Difficulty\t" + string(difficulty))
+		body := fmt.Sprintf("tree %s\nparent %s\nauthor CTF %s %d\n\nI Can haz gitcoin?\n\nCounter:%d",
+			tree, head, user, unix, counter)
+
+		sha1 := getSha1(i, body)
+		ofInterest := sha1[:len(difficulty)]
 
 		if bytes.Compare(ofInterest, difficulty) < 0 {
-			fmt.Println("Complete")
+			fmt.Println("Mined a gitcoin with ", sha1)
+			gitReset(i, string(sha1))
+			iStr := strconv.Itoa(i)
+			comm <- "Found it! in level_" + iStr + string(sha1)
 			break
+		}
+		if counter%500 == 0 {
+			fmt.Println(string(body))
+			fmt.Println(string(sha1))
+			fmt.Println(string(ofInterest))
+			fmt.Println(string(difficulty))
+		}
+	}
+
+}
+
+func main() {
+	clone()
+	comm := make(chan string)
+	quit := make(chan bool)
+	for i := 0; i < 20; i++ {
+		copyRepo(i)
+		updateLedger(i)
+		gitAddLedger(i)
+		//resetHead()
+		head := getHeadHash(i)
+		difficulty := getDifficulty(i)
+
+		go mine(comm, quit, i, head, difficulty)
+	}
+	for {
+		select {
+		case info := <-comm:
+			fmt.Println(info)
+			close(quit)
 		}
 	}
 }
